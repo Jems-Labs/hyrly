803280b7043fee28c4f56c4bbf3567f702efdbd6
Krish Soni <isonikrish@gmail.com>
2025-03-13 12:13:56 +0530

    testing ci/cd

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -31,7 +31,7 @@ userRoutes.delete(
 );
 userRoutes.get("/notifications", protectRoute, handleGetMyNotifications);
 userRoutes.delete("/notification/:id", protectRoute, handleDeleteNotification);
-userRoutes.get("/leaderboard",protectRoute, handleGetLeaderboard);
+userRoutes.get("/leaderboard", handleGetLeaderboard);
 userRoutes.get("/public-user/:id", protectRoute, handleGetPublicUser);
 
 
